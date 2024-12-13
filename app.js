6a2ac039edeab9388dd2bcafc089a72024804260
wisdom Agha <wisdomcool11@gmail.com>
2024-12-13 02:25:51 +0100

    6.2: added aoi animation efferct on scroll to home page, about page and contact page

--- app.js
+++ app.js
@@ -13,18 +13,21 @@ toggle_bar.addEventListener('click' , function(){
 
 
 // Scroll Reveal
-ScrollReveal({ 
-    reset: true, 
-    distance: '60px',
-    duration: 2500,
-    delay: 400
-});
+// ScrollReveal({ 
+//     reset: true, 
+//     distance: '60px',
+//     duration: 2500,
+//     delay: 400
+// });
 
-ScrollReveal().reveal('header', {delay: 200, origin: 'top'});
-ScrollReveal().reveal('#hero__text, #hero__content, #CTA__btn',{delay: 600, origin: 'left', interval: 400 })
-ScrollReveal().reveal('#card__one, #card__two',{delay: 500, origin: 'bottom' , interval: 200 })
+// ScrollReveal().reveal('header', {delay: 200, origin: 'top'});
+// ScrollReveal().reveal('#hero__text, #hero__content, #CTA__btn',{delay: 600, origin: 'left', interval: 400 })
+// ScrollReveal().reveal('#card__one, #card__two',{delay: 500, origin: 'bottom' , interval: 200 })
 
 
+// AOS animation on scroll function calling
+
+AOS.init();
 
 
 // Accordion
@@ -39,4 +42,94 @@ faqs.forEach(faq =>{
     })
 })
 
+
+// contact form validation
+
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email')
+const subject = document.querySelector('#subject');
+const textArea = document.querySelector('#textArea');
+
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    validateInput ();
+})
+
+const setError = (element, message) =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+}
+
+const setSuccess = element =>{
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.remove('error');
+    inputControl.classList.add('success');
+}
+
+const isValidEmail = email =>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(String(email).toLowerCase());
+}
+
+
+const validateInput = ()=>{
+    const userValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const subValue = subject.value.trim();
+    const textArValue = textArea.value.trim();
+    
+    // conditions
+    if(userValue === ''){
+        setError(username, 'field is required*');
+
+    }else{
+        setSuccess(username);
+    }
+
+    // email
+    if(emailValue === ''){
+        setError(email, 'field is required*');
+
+    }else if(!isValidEmail(emailValue)){
+        setError(email, 'not a valide email')
+
+    }else{
+        setSuccess(email)
+    }
+
+    // subject
+    if(subValue === ''){
+        setError(subject, 'field is required*')
+
+    }else{
+        setSuccess(subject)
+    }
+
+    // textAreas
+    if(textArValue === ''){
+        setError(textArea, 'kindly fill this field, to let us know your request');
+    }else{
+        setSuccess(textArea);
+    }
+
+    emptyInput(); // not working yet
+
+}
+
+const emptyInput = ()=>{
+    username.innerText = '';
+    email.innerText = '';
+    subject.innerText = '';
+    textArea.innerText = ''
+}
 
---
+++ validateInputs.js
@@ -0,0 +1,8 @@
+const validateInputs = () => {
+    const userValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value.trim();
+    const textArea = textArea.value.trim();
+
+
+};
