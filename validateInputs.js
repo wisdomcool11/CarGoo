b0be49adf7110b51f772eeba639f84b93581bd70
wisdom Agha <wisdomcool11@gmail.com>
2024-12-16 19:04:11 +0100

    6.3: compress the website images

--- validateInputs.js
+++
@@ -1,8 +0,0 @@
-const validateInputs = () => {
-    const userValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const subjectValue = subject.value.trim();
-    const textArea = textArea.value.trim();
-
-
-};
